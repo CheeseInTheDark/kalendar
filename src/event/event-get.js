b572e096154ff1cf93d7bdbc3a4f064cbd082977
CheeseInTheDark <blockbattle@gmail.com>
2019-05-31 22:58:37 -0400

    Remove unneeded parantheses

--- src/event/event-get.js
+++ src/event/event-get.js
@@ -1,7 +1,7 @@
 const eventIdToEvent = require('./event-id-to-event')
 
 module.exports = function getEvent(req, res) {
-    const event = eventIdToEvent((req.params.eventId))
+    const event = eventIdToEvent(req.params.eventId)
 
     res.setHeader('Content-Type', 'application/json')
     res.status(200).send(event)
